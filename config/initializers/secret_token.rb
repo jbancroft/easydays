9e07a9b176f74e9af532dcdf0f68d6c018aa0193
Jim Bancroft <jbancroft@wistia.com>
2010-10-18 22:24:43 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Easydays::Application.config.secret_token = 'b50b21371447e407d5d9dd1ee3c513ac03ca6c2ecb06546ac5e58ebe3161fce542840db9870bbdf3fd3a2859b55386466de7cc440edb0bff4faf491c9da8f45a'
